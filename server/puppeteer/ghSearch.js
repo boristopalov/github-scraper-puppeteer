04edbeaa37ad3b3ed69f83d7e2dc043b04afc37b
Boris <boristopalov1@gmail.com>
2022-03-14 15:34:33 -0400

    start scrape from searching for repo or users

--- server/puppeteer/ghSearch.js
+++ server/puppeteer/ghSearch.js
@@ -3,12 +3,14 @@ import axios from "axios";
 import { scrapeUserProfile } from "./scrapeUserProfile.js";
 import { MongoClient, ServerApiVersion } from "mongodb";
 import dotenv from "dotenv";
+import { scrapeRepo } from "./scrapeRepo.js";
 dotenv.config({ path: "../.env" });
 const TOKEN = process.env.TOKEN;
 
 const ghSearch = async (query, type, db) => {
   // add in options for users or repos or other types
   // error handling ?
+  // rate limit: 30 requests per minute since we are authenticated
   const url = `https://api.github.com/search/${type}?q=${query}`;
   const res = await axios
     .get(url, {
@@ -20,23 +22,32 @@ const ghSearch = async (query, type, db) => {
     .catch((error) => console.log(error.message));
   if (res.data) {
     const data = res.data;
-    // console.log(data);
-    for (const item of data.items) {
-      const userUrl = item.html_url;
+    if (type === "repositories") {
+      for (const item of data.items) {
+        const url = item.html_url;
+        if (!(await db.collection("scraped_repos").findOne({ url: url }))) {
+          await db.collection("scraped_repos").insertOne({ url: url });
+          const browser = await puppeteer.launch({ headless: true });
+          const page = await browser.newPage();
+          await page.goto(repoUrl);
+          console.log(`${repoUrl} not found in DB, scraping it...`);
+          await scrapeRepo(browser, page, db);
+        }
+      }
+    }
+    if (type === "user") {
       const username = item.login;
       if (
         !(await db.collection("scraped_users").findOne({ username: username }))
       ) {
         await db.collection("scraped_users").insertOne({ username: username });
         console.log(`${username} not found in DB, scraping the user...`);
-        await scrapeUserProfile(userUrl, true, db);
-        // puppeteerData.push(puppeteerUserData);
-        // console.log(puppeteerData);
+        await scrapeUserProfile(item.html_url, true, db);
       }
     }
+
     console.log("all done!");
     return;
-    // return puppeteerData;
   } else {
     console.log("no users to scrape!");
     return;
@@ -44,10 +55,9 @@ const ghSearch = async (query, type, db) => {
 };
 
 dotenv.config();
-// dbUser = process.env.DB_USER;
-// dbPass = process.env.DB_PASS;
 
 const uri = process.env.URI;
+
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
