75b071d0e924f1ba664395909843b22340bc4243
Boris <boristopalov1@gmail.com>
2022-07-15 19:49:49 +0300

    add wrapper for connecting to DB

---
+++ server/utils/dbConnect.js
@@ -0,0 +1,18 @@
+import { MongoClient, ServerApiVersion } from "mongodb";
+import dotenv from "dotenv";
+dotenv.config({ path: "../.env" });
+const uri = process.env.URI;
+
+export const mongoClient = async () => {
+  try {
+    const client = new MongoClient(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      serverApi: ServerApiVersion.v1,
+    });
+    await client.connect();
+    return client;
+  } catch (e) {
+    console.error(e);
+  }
+};
