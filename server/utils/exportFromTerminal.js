e836c92424983af21c9fc21e50d12286571347f4
Boris <boristopalov1@gmail.com>
2022-07-16 16:19:15 +0300

    fix yarn export so that you can export all available users as well (like before)

--- server/utils/exportFromTerminal.js
+++ server/utils/exportFromTerminal.js
@@ -1,5 +1,6 @@
 import { mongoClient } from "./dbConnect.js";
 import { exec } from "child_process";
+import { csvExport } from "./csvExport.js";
 
 const exportRepo = async (db, url) => {
   const toExport = await db
@@ -132,16 +133,18 @@ const exportUser = async (db, _url) => {
 };
 
 const exportFromTerminal = async () => {
-  if (process.argv.length < 4) {
-    console.error("Usage: yarn export ['repo' | 'org' | 'user'] [URL]");
-    process.exit(1);
-  }
-  const type = process.argv[2];
-  const url = process.argv[3];
-
   try {
     const client = await mongoClient();
     const db = client.db("scraper");
+    if (process.argv.length === 2) {
+      csvExport(db);
+      return;
+    } else if (process.argv.length < 4) {
+      console.error("Usage: yarn export ['repo' | 'org' | 'user'] [URL]");
+      process.exit(1);
+    }
+    const type = process.argv[2];
+    const url = process.argv[3];
     if (type === "repo") {
       exportRepo(db, url);
     } else if (type === "org") {
