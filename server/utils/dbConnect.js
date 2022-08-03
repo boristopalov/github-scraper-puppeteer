01f18c5e0efccc05d42738d5be57cb30d2198dc1
Boris <boristopalov1@gmail.com>
2022-08-02 21:44:59 -0400

    fix DB config helper function's .env path

--- server/utils/dbConnect.js
+++ server/utils/dbConnect.js
@@ -1,6 +1,6 @@
 import { MongoClient, ServerApiVersion } from "mongodb";
 import dotenv from "dotenv";
-dotenv.config({ path: "../.env" });
+dotenv.config();
 const uri = process.env.URI;
 
 export const mongoClient = async () => {
