52252d486d28f614616c65f3909a471857ba5865
Boris <boristopalov1@gmail.com>
2022-08-10 23:59:25 -0400

    clean up logic for checking if scraped

--- server/utils/scrapeCheck/checkIfScrapedTerminal.js
+++ server/utils/scrapeCheck/checkIfScrapedTerminal.js
@@ -13,7 +13,7 @@ const checkIfFullyScraped = () => {
     );
     process.exit(1);
   }
-  dotenv.config({ path: "../.env" });
+  dotenv.config({ path: "../../.env" });
   const uri = process.env.URI;
   const client = new MongoClient(uri, {
     useNewUrlParser: true,
@@ -22,26 +22,22 @@ const checkIfFullyScraped = () => {
   });
   client.connect(async (err) => {
     if (err) {
-      console.error(err);
-      process.exit(1);
+      return err;
     }
     const db = client.db("scraper");
     const type = process.argv[2];
     const url = process.argv[3];
-    let scraped;
     if (type === "org") {
-      scraped = await checkIfOrgScraped(db, url);
+      await checkIfOrgScraped(db, url);
     } else if (type === "repo") {
-      scraped = await checkIfRepoScraped(db, url);
+      await checkIfRepoScraped(db, url);
     } else if (type === "user") {
-      scraped = await checkIfUserScraped(db, url);
+      await checkIfUserScraped(db, url);
     } else {
       console.log("only possible types are 'org', 'repo', and 'user'");
-      process.exit(1);
     }
     await client.close();
-    if (scraped) return true;
-    return false;
+    return;
   });
 };
 
