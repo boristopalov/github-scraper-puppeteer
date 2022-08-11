a78b1294f021311192be8a8a3cd3e559798bea32
Boris <boristopalov1@gmail.com>
2022-08-10 23:55:07 -0400

    fix export so it looks for URLs instead of incorrectly looking for objects in the DB

--- server/utils/export/export.js
+++ server/utils/export/export.js
@@ -1,134 +1,148 @@
 import { exec } from "child_process";
+import arrayOfObjectsToCSV from "../arrayOfObjectsToCSV.js";
+import fs from "fs";
 
 export const exportRepo = async (db, url) => {
-  const toExport = await db
-    .collection("users")
-    .find({ repoCommits: url })
-    .toArray();
-  if (!toExport || toExport.length === 0) {
-    console.error("No record(s) with that URL found");
+  const repo = await db.collection("repos").findOne({ url });
+  if (!repo) {
+    console.error("No repo with that URL found");
     process.exit(1);
   }
-  const urls = toExport.map((e) => e.url);
+  const contributors = repo.contributors;
+  const toExport = await db
+    .collection("users")
+    .find(
+      {
+        $and: [
+          { queuedTasks: 0 },
+          { exported: false },
+          { githubUrl: { $in: contributors } },
+        ],
+      },
+      { _id: 0, repoCommits: 0, queuedTasks: 0, exported: 0, createdAt: 0 }
+    )
+    .toArray();
+  // console.log(toExport);
+  const csvString = arrayOfObjectsToCSV(toExport);
   const date = Date.now();
-
-  const command = `mongoexport --config='./exportConfig.yaml' --collection='users' --type='csv' --fields='username,name,email,location,isInNewYork,bio,bioMatchesKeywords,githubUrl,numPullRequestReposWithHundredStars,numPullRequestReposWithReadmeKeywordMatch,contributionCount,tenStarRepoCount,isUserReadmeKeywordMatch,company,userCompanyIsOrg,githubFollowers,githubFollowing,numOrgBioKeywordMatch,numOrgReposWithHundredStars,numOrgReposReadmeKeywordMatch' --query='{"$and": [{"queuedTasks": 0 }, {"exported": false}, {"url": {$in: ${urls}}} ]}' --out='data/scraped_users_${date}.csv'`;
-  exec(
-    command,
-
-    async (err, stdout, stderr) => {
-      if (err) {
-        // node couldn't execute the command
-        console.error(err.stack);
-        process.exit(1);
-      }
-      // the *entire* stdout and stderr (buffered)
-      console.log(`stdout: ${stdout}`);
-      console.log(`stderr: ${stderr}`);
-      const updatedDoc = {
-        $set: {
-          exported: true,
-          updatedAt: Date.now(),
-        },
-      };
-      await db.collection("users").updateMany(
-        {
-          $and: [
-            { queuedTasks: 0 },
-            { exported: false },
-            { url: { $in: urls } },
-          ],
-        },
-        updatedDoc
-      );
+  fs.writeFile(`../../data/scraped_users_${date}.csv`, csvString, async (e) => {
+    if (e) {
+      console.error(e);
+      process.exit(1);
     }
+  });
+  const updatedDoc = {
+    $set: {
+      exported: true,
+      updatedAt: date,
+    },
+  };
+  await db.collection("users").updateMany(
+    {
+      $and: [
+        { queuedTasks: 0 },
+        { exported: false },
+        { githubUrl: { $in: contributors } },
+      ],
+    },
+    updatedDoc
   );
-  return `scraped_users_${date}.csv`;
+  console.log(`Wrote to ../../data/scraped_users_${date}.csv`);
+  process.exit(0);
 };
 
 export const exportOrg = async (db, url) => {
   const org = await db.collection("orgs").findOne({ url });
+  const date = Date.now();
   if (!org) {
     console.error("No record(s) with that URL found");
     process.exit(1);
   }
   const reposInOrg = org.reposInOrg;
-  const toExport = await db
-    .collection("users")
-    .find({ repoCommits: { $in: reposInOrg } })
-    .toArray();
-  const urls = toExport.map((e) => e.url);
-  const date = Date.now();
-
-  const command = `mongoexport --config='./exportConfig.yaml' --collection='users' --type='csv' --fields='username,name,email,location,isInNewYork,bio,bioMatchesKeywords,githubUrl,numPullRequestReposWithHundredStars,numPullRequestReposWithReadmeKeywordMatch,contributionCount,tenStarRepoCount,isUserReadmeKeywordMatch,company,userCompanyIsOrg,githubFollowers,githubFollowing,numOrgBioKeywordMatch,numOrgReposWithHundredStars,numOrgReposReadmeKeywordMatch' --query='{"$and": [{"queuedTasks": 0 }, {"exported": false}, {"url": {$in: ${urls}}} ]}' --out='data/scraped_users_${date}.csv'`;
-  exec(
-    command,
-
-    async (err, stdout, stderr) => {
-      if (err) {
-        // node couldn't execute the command
-        console.error(err.stack);
-        process.exit(1);
-      }
-      // the *entire* stdout and stderr (buffered)
-      console.log(`stdout: ${stdout}`);
-      console.log(`stderr: ${stderr}`);
-      const updatedDoc = {
-        $set: {
-          exported: true,
-          updatedAt: Date.now(),
-        },
-      };
-      await db.collection("users").updateMany(
+  let fullCsvString = "";
+  for (const url of reposInOrg) {
+    const repo = await db.collection("repos").findOne({ url });
+    if (!repo) {
+      console.log(`no repo with ${url} found`);
+      continue;
+    }
+    const contributors = repo.contributors;
+    const toExport = await db
+      .collection("users")
+      .find(
         {
           $and: [
             { queuedTasks: 0 },
             { exported: false },
-            { url: { $in: urls } },
+            { githubUrl: { $in: contributors } },
           ],
         },
-        updatedDoc
-      );
+        { _id: 0, repoCommits: 0, queuedTasks: 0, exported: 0, createdAt: 0 }
+      )
+      .toArray();
+    const repoCsvString = arrayOfObjectsToCSV(toExport);
+    fullCsvString += repoCsvString;
+    const updatedDoc = {
+      $set: {
+        exported: true,
+        updatedAt: date,
+      },
+    };
+    await db.collection("users").updateMany(
+      {
+        $and: [
+          { queuedTasks: 0 },
+          { exported: false },
+          { githubUrl: { $in: contributors } },
+        ],
+      },
+      updatedDoc
+    );
+  }
+  fs.writeFile(
+    `../../data/scraped_users_${date}.csv`,
+    fullCsvString,
+    async (e) => {
+      if (e) {
+        console.error(e);
+        process.exit(1);
+      }
     }
   );
-  return `scraped_users_${date}.csv`;
+  console.log(`Wrote to ../../data/scraped_users_${date}.csv`);
+  process.exit(0);
 };
 
-export const exportUser = async (db, _url) => {
-  const user = await db.collection("users").findOne({ _url });
+export const exportUser = async (db, url) => {
+  const user = await db.collection("users").findOne({ githubUrl: url });
   if (!user) {
     console.error("No record(s) with that URL found");
     process.exit(1);
   }
-  const url = user.url;
+  const toExport = [user];
+  const csvString = arrayOfObjectsToCSV(toExport);
   const date = Date.now();
 
-  const command = `mongoexport --config='./exportConfig.yaml' --collection='users' --type='csv' --fields='username,name,email,location,isInNewYork,bio,bioMatchesKeywords,githubUrl,numPullRequestReposWithHundredStars,numPullRequestReposWithReadmeKeywordMatch,contributionCount,tenStarRepoCount,isUserReadmeKeywordMatch,company,userCompanyIsOrg,githubFollowers,githubFollowing,numOrgBioKeywordMatch,numOrgReposWithHundredStars,numOrgReposReadmeKeywordMatch' --query='{"$and": [{"queuedTasks": 0 }, {"exported": false}, {"url": ${url}} ]}' --out='data/scraped_users_${date}.csv'`;
-  exec(
-    command,
-
-    async (err, stdout, stderr) => {
-      if (err) {
-        // node couldn't execute the command
-        console.error(err.stack);
-        process.exit(1);
-      }
-      // the *entire* stdout and stderr (buffered)
-      console.log(`stdout: ${stdout}`);
-      console.log(`stderr: ${stderr}`);
-      const updatedDoc = {
-        $set: {
-          exported: true,
-          updatedAt: Date.now(),
-        },
-      };
-      await db.collection("users").updateMany(
-        {
-          $and: [{ queuedTasks: 0 }, { exported: false }, { url: url }],
-        },
-        updatedDoc
-      );
+  fs.writeFile(`../../data/scraped_users_${date}.csv`, csvString, async (e) => {
+    if (e) {
+      console.error(e);
+      process.exit(1);
     }
+  });
+
+  const updatedDoc = {
+    $set: {
+      exported: true,
+      updatedAt: date,
+    },
+  };
+  await db.collection("users").updateOne(
+    {
+      $and: [{ queuedTasks: 0 }, { exported: false }, { githubUrl: url }],
+    },
+    updatedDoc
   );
-  return `scraped_users_${date}.csv`;
+
+  console.log(`Wrote to ../../data/scraped_users_${date}.csv`);
+  process.exit(0);
 };
--- server/utils/export/exportFromTerminal.js
+++ server/utils/export/exportFromTerminal.js
@@ -1,11 +1,22 @@
-import { mongoClient } from "../dbConnect.js";
 import { csvExport } from "../csvExport.js";
 import { exportRepo, exportOrg, exportUser } from "./export.js";
+import dotenv from "dotenv";
+import { MongoClient, ServerApiVersion } from "mongodb";
 
 const exportFromTerminal = async () => {
-  try {
-    const client = await mongoClient();
+  dotenv.config({ path: "../../.env" });
+  const uri = process.env.URI;
+  const client = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverApi: ServerApiVersion.v1,
+  });
+  client.connect(async (err) => {
+    if (err) {
+      return err;
+    }
     const db = client.db("scraper");
+
     if (process.argv.length === 2) {
       csvExport(db);
       return;
@@ -16,19 +27,16 @@ const exportFromTerminal = async () => {
     const type = process.argv[2];
     const url = process.argv[3];
     if (type === "repo") {
-      exportRepo(db, url);
+      await exportRepo(db, url);
     } else if (type === "org") {
-      exportOrg(db, url);
+      await exportOrg(db, url);
     } else if (type === "user") {
-      exportUser(db, url);
+      await exportUser(db, url);
     } else {
       console.error("Possible types are: 'repo', 'org', or 'user'");
       process.exit(1);
     }
-  } catch (e) {
-    console.error(e);
-    process.exit(1);
-  }
+  });
 };
 
 exportFromTerminal();
