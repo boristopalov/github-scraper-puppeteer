aace750db4109cac9f331927f20b8fea2bf0206f
Boris <boristopalov1@gmail.com>
2022-10-06 16:08:43 -0400

    use mongoClient() helper function in checkIfScrapedTerminal.js

--- server/utils/scrapeCheck/checkIfScrapedTerminal.js
+++ server/utils/scrapeCheck/checkIfScrapedTerminal.js
@@ -1,44 +1,33 @@
-import dotenv from "dotenv";
-import { MongoClient, ServerApiVersion } from "mongodb";
 import {
   checkIfOrgScraped,
   checkIfRepoScraped,
   checkIfUserScraped,
 } from "./checkIfScraped.js";
+import { mongoClient } from "../dbConnect.js";
+import { DB_ENV } from "../../constants/constants.js";
 
-const checkIfFullyScraped = () => {
+const checkIfFullyScraped = async () => {
   if (process.argv.length < 4) {
     console.log(
       "Error- please provide a type ('repo', 'user', or 'org') and URL"
     );
     process.exit(1);
   }
-  dotenv.config({ path: "../../.env" });
-  const uri = process.env.URI;
-  const client = new MongoClient(uri, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    serverApi: ServerApiVersion.v1,
-  });
-  client.connect(async (err) => {
-    if (err) {
-      return err;
-    }
-    const db = client.db("scraper");
-    const type = process.argv[2];
-    const url = process.argv[3];
-    if (type === "org") {
-      await checkIfOrgScraped(db, url);
-    } else if (type === "repo") {
-      await checkIfRepoScraped(db, url);
-    } else if (type === "user") {
-      await checkIfUserScraped(db, url);
-    } else {
-      console.log("only possible types are 'org', 'repo', and 'user'");
-    }
-    await client.close();
-    return;
-  });
+  const client = await mongoClient();
+  const db = DB_ENV === "testing" ? client.db("testing") : client.db("scraper");
+  const type = process.argv[2];
+  const url = process.argv[3];
+  if (type === "org") {
+    await checkIfOrgScraped(db, url);
+  } else if (type === "repo") {
+    await checkIfRepoScraped(db, url);
+  } else if (type === "user") {
+    await checkIfUserScraped(db, url);
+  } else {
+    console.log("only possible types are 'org', 'repo', and 'user'");
+  }
+  await client.close();
+  return;
 };
 
 checkIfFullyScraped();
