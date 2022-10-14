70a6b6a2e03f232c340315427ed30c26416206c5
Boris <boristopalov1@gmail.com>
2022-10-13 21:44:07 -0400

    return an object with additional data in checkIfScraped instead of just a boolean

--- server/utils/scrapeCheck/checkIfScraped.js
+++ server/utils/scrapeCheck/checkIfScraped.js
@@ -1,57 +1,54 @@
 export const checkIfOrgScraped = async (db, url) => {
   const org = await db.collection("orgs").findOne({ url });
   if (!org) {
-    console.log(`${url} not found in DB.`);
-    return false;
+    return { url, scraped: false, tasks: null };
   }
-  if (!Object.prototype.hasOwnProperty.call(org, "reposInOrg")) {
-    console.log(`No repos logged for ${url}`);
-    return false;
+  if (
+    Object.prototype.hasOwnProperty.call(org, "queuedTasksArray") &&
+    org.queuedTasksArray.length > 0
+  ) {
+    return { url, scraped: false, tasks: org.queuedTasksArray };
   }
   for (const repoUrl of org.reposInOrg) {
     const repoScraped = await checkIfRepoScraped(db, repoUrl);
-    if (!repoScraped) {
-      console.log(
-        `${repoUrl} is a repo in ${url} and hasn't been fully scraped yet.`
-      );
-      return false;
+    if (!repoScraped.scraped) {
+      return repoScraped;
     }
   }
-  console.log(`${url} has finished scraping.`);
-  return true;
+  return { url, scraped: true, tasks: [] };
 };
 
 export const checkIfRepoScraped = async (db, url) => {
   const repo = await db.collection("repos").findOne({ url });
   if (!repo) {
-    console.log(`${url} not found in DB.`);
-    return false;
+    return { url, scraped: false, tasks: null };
   }
-  if (!Object.prototype.hasOwnProperty.call(repo, "contributors")) {
-    console.log(`No contributors logged for ${url}`);
-    return false;
+  if (
+    Object.prototype.hasOwnProperty.call(repo, "queuedTasksArray") &&
+    repo.queuedTasksArray.length > 0
+  ) {
+    return { url, scraped: false, tasks: repo.queuedTasksArray };
   }
   for (const { githubUrl } of repo.contributors) {
-    const scraped = await checkIfUserScraped(db, githubUrl);
-    if (!scraped) {
-      return false;
+    const userScraped = await checkIfUserScraped(db, githubUrl);
+    if (!userScraped.scraped) {
+      return userScraped;
     }
   }
   console.log(`${url} has finished scraping.`);
-  return true;
+  return { url, scraped: true, tasks: [] };
 };
 
 export const checkIfUserScraped = async (db, url) => {
   const user = await db.collection("users").findOne({ githubUrl: url });
   if (!user) {
-    console.log(`${url} not found in DB.`);
-    return false;
+    return { url, scraped: false, tasks: null };
   }
   if (
-    Object.prototype.hasOwnProperty.call(user, "queuedTasks") &&
-    user.queuedTasks > 0
+    Object.prototype.hasOwnProperty.call(user, "queuedTasksArray") &&
+    user.queuedTasksArray.length > 0
   ) {
-    return false;
+    return { url, scraped: false, tasks: user.queuedTasksArray };
   }
-  return true;
+  return { url, scraped: true, tasks: [] };
 };
