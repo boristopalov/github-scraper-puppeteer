4b56212625dab2bc2234e2179d072d1244cbcaaa
Boris <boristopalov1@gmail.com>
2022-10-14 14:07:47 -0400

    increase connctions by 1 in isScraperActive() since we are pinging the server, and add a check for STOP_SCRAPER_FLAG

--- server/utils/isScraperActive.js
+++ server/utils/isScraperActive.js
@@ -1,6 +1,8 @@
+import { STOP_SCRAPER_FLAG } from "../puppeteer/stopScraperFlag.js";
+
 export const isScraperActive = async (db) => {
   const adminDb = db.admin();
   const { connections } = await adminDb.serverStatus();
   console.log(connections);
-  return connections.current > 3; // >3 because atlas makes some connections other than just with the client
+  return connections.current > 4 && !STOP_SCRAPER_FLAG; // >3 because of ping(), and atlas makes some connections other than just with the client
 };
