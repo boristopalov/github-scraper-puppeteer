5bca629fb89d68f53af3b87047a7ee86a95ee2ff
Boris <boristopalov1@gmail.com>
2022-10-18 11:24:54 -0400

    have separate scraper toggling functions for more fine control

--- server/puppeteer/stopScraperFlag.js
+++ server/puppeteer/stopScraperFlag.js
@@ -1,4 +1,8 @@
 export let STOP_SCRAPER_FLAG = false;
-export function toggleScraperFlag() {
-  STOP_SCRAPER_FLAG = !STOP_SCRAPER_FLAG;
+export function stopScraperFlag() {
+  STOP_SCRAPER_FLAG = false;
+}
+
+export function startScraperFlag() {
+  STOP_SCRAPER_FLAG = true;
 }
