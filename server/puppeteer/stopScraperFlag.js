0382cb063667f9154f4f09c00848bd9477c6d08d
Boris <boristopalov1@gmail.com>
2022-10-18 21:23:43 -0400

    fix functions in stopScraperFlag()

--- server/puppeteer/stopScraperFlag.js
+++ server/puppeteer/stopScraperFlag.js
@@ -1,8 +1,8 @@
 export let STOP_SCRAPER_FLAG = false;
 export function stopScraperFlag() {
-  STOP_SCRAPER_FLAG = false;
+  STOP_SCRAPER_FLAG = true;
 }
 
 export function startScraperFlag() {
-  STOP_SCRAPER_FLAG = true;
+  STOP_SCRAPER_FLAG = false;
 }
