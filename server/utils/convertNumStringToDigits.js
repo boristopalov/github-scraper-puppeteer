c52cdc3fdd3860aef856ec20d8422e222151b1cc
Boris <boristopalov1@gmail.com>
2022-11-02 12:57:13 -0400

    get rid of commas when converting a string to an int

--- server/utils/convertNumStringToDigits.js
+++ server/utils/convertNumStringToDigits.js
@@ -1,7 +1,7 @@
 export default function convertNumStringToDigits(numString) {
   const thousand = "K";
   const million = "M";
-  const parsed = parseInt(numString);
+  const parsed = parseInt(numString.replace(",", ""));
   if (numString.toUpperCase().includes(thousand)) return parsed * 1000;
   else if (numString.toUpperCase().includes(million)) return parsed * 1000000;
   else return parsed;
