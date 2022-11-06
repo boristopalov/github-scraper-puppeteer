53a678d797d518df920c856f94eda24c8656493f
Boris <boristopalov1@gmail.com>
2022-11-06 17:59:37 -0500

    fix bot detection check

--- server/src/scrape/checkForBotDetection.js
+++ server/src/scrape/checkForBotDetection.js
@@ -2,23 +2,27 @@ import sleep from "../utils/sleep.js";
 
 const checkForBotDetection = async (page) => {
   let sleepMultiplier = 2;
-  while (
-    (await page.waitForXPath(
-      '//div[@class="c" and .//*[contains(text(), "Whoa there!")]]',
-      {
-        timeout: 2000,
-      }
-    )) !== null
-  ) {
-    console.log(
-      "Abuse detection mechanism detected- waiting ",
-      sleepMultiplier,
-      " minutes before trying again."
-    );
-    // 1 minute * sleepMultiplier
-    await sleep(60000 * sleepMultiplier);
-    await page.reload();
-    sleepMultiplier *= 2;
+  try {
+    while (
+      (await page.waitForXPath(
+        '//div[@class="c" and .//*[contains(text(), "Whoa there!")]]',
+        {
+          timeout: 2000,
+        }
+      )) !== null
+    ) {
+      console.log(
+        "Abuse detection mechanism detected- waiting ",
+        sleepMultiplier,
+        " minutes before trying again."
+      );
+      // 1 minute * sleepMultiplier
+      await sleep(60000 * sleepMultiplier);
+      await page.reload();
+      sleepMultiplier *= 2;
+    }
+  } catch (e) {
+    return;
   }
 };
 
